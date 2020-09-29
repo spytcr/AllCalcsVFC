638c57e25060bbb8c7a08da10d9d7d03478c8599
SashaCyberPrank17 <SashaCyberPrank17@users.noreply.github.com>
2020-09-29 16:57:59 +0200

    Updates

---
+++ precache-manifest.16fb6373dbd0244d43e9b7c2c90d8fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ebf90cb0f6fb2eda3fbd8ac164154a",
+    "url": "/spytcr/AllCalcsVFC/index.html"
+  },
+  {
+    "revision": "8b44d8ccd1d776be9095",
+    "url": "/spytcr/AllCalcsVFC/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "5f10a2f9cdfe4f356233",
+    "url": "/spytcr/AllCalcsVFC/static/css/main.2f023da6.chunk.css"
+  },
+  {
+    "revision": "8b44d8ccd1d776be9095",
+    "url": "/spytcr/AllCalcsVFC/static/js/2.c35ae979.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/spytcr/AllCalcsVFC/static/js/2.c35ae979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f10a2f9cdfe4f356233",
+    "url": "/spytcr/AllCalcsVFC/static/js/main.7b50cb4a.chunk.js"
+  },
+  {
+    "revision": "6d3ac02418b910d07040",
+    "url": "/spytcr/AllCalcsVFC/static/js/runtime-main.11e1e75b.js"
+  }
+]);
